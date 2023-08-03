ae6daefcc4e35ad85bb0eb739f45a8d68927cc18
Meigan Luiz Uliana <luiz_uliana@hotmail.com>
2023-08-03 17:43:30 +0000

    commit

--- models/customers.sql
+++ models/customers.sql
@@ -1,7 +1,15 @@
+with markup as (
+
+
 select *, 
 first_value(customer_id)
 over(partition by company_name, contact_name
 order by company_name
 rows between unbounded preceding and unbounded following) as result
 from {{source('sources','customers')}}
-
+), removed as (
+    select distinct result from markup 
+), final as (
+    select * from {{source('sources','customers')}} where customer_id in (select result from removed)
+)
+select * from final 

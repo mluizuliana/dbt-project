ab2300fa5bf926d4d75b9bfe7c14ad569f4b37f9
Meigan Luiz Uliana <luiz_uliana@hotmail.com>
2023-08-04 12:20:46 +0000

    employees commit

---
+++ models/employees.sql
@@ -0,0 +1,8 @@
+with calc_employees AS (
+    select 
+    date_part(year, current_date) - date_part(year,birth_date) age, /*Calcular idade*/
+    date_part(year, current_date) - date_part(year, hire_date) lenghtofservice, /*Tempo de trabalho*/
+    first_name || ' ' || last_name name, * /*Junção das tabelas*/
+    from {{source('sources','employees')}}
+)
+select * from calc_employees

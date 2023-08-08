2217c49f6278c814809cb01f1472859bfabf701c
Meigan Luiz Uliana <luiz_uliana@hotmail.com>
2023-08-08 11:30:07 +0000

    commit bireport

--- models/bireport.sql
+++ models/bireport.sql
@@ -1,7 +1,10 @@
 {{
     config(
         materialized='table', 
-        post_hook=["
+        pre_hook=["begin; lock table {{target.schema}}.bireport; 
+        "]
+        post_hook=["commit;
+            GRANT USAGE ON SCHEMA {{target.schema}} TO GROUP biusers;
             GRANT SELECT ON TABLE {{target.schema}}.bireport TO GROUP biusers;
         "]
     )
